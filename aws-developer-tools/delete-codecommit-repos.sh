98fffc6d6a70b3edb411a6cbcf10b9fe19a86d44
codigube <codigube@gmail.com>
2020-12-12 22:59:33 +0100

    Added script for deleting codecommit repositories

---
+++ aws-developer-tools/delete-codecommit-repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Enter the keyword for finding repositories to be deleted:"
+read keyword
+
+for k in $(aws codecommit list-repositories | jq -r '.repositories[] | select(.repositoryName | contains("'$keyword'")) | .repositoryName'); do
+    output=$(aws codecommit delete-repository --repository-name "$k");
+    if [ $? -eq 0 ]; then
+        echo ">>> repository deletion SUCCEED: $k"
+    fi
+done
